b21898da8b076d7aaf87fa35451865ae3c243c90
nanaios <game.ryd.131@gmail.com>
2026-02-23 18:05:40 +0900

    mixinの改善

--- src/main/java/com/nanaios/applied_ammo_box/mixin/aeinfinitybooster/MixinAE2LinkHelper.java
+++ src/main/java/com/nanaios/applied_ammo_box/mixin/aeinfinitybooster/MixinAE2LinkHelper.java
@@ -12,21 +12,19 @@ import uk.co.hexeption.aeinfinitybooster.setup.ModItems;
 
 @Mixin(value = AE2LinkHelper.class, remap = false)
 public class MixinAE2LinkHelper {
-    @Inject(method = "getWapSqDistance", at = @At("HEAD"), cancellable = true)
-    private static void applied_ammo_box$mixinGetWapSqDistance(WirelessAccessPointBlockEntity wap, BlockPos pos, Level level, CallbackInfoReturnable<Double> cir) {
-        // Dimensionカード装着時はディメンションを問わず1024mに設定
-        if (wap.getInternalInventory().getStackInSlot(0).is(ModItems.DIMENSION_CARD.get())) {
-            cir.setReturnValue(1024.0D);
-        }
-
-        //  Dimensionカード装着時以外は次元が違う場合は処理しない
-        if (wap.getLocation().getLevel() != level) {
-            return;
-        }
-
+    @Inject(method = "getWapSqDistance", at = @At(value = "INVOKE", target = "appeng/blockentity/networking/WirelessAccessPointBlockEntity.getRange()D"), cancellable = true)
+    private static void applied_ammo_box$mixinGetWapSqDistance$infinityCardInject(WirelessAccessPointBlockEntity wap, BlockPos pos, Level level, CallbackInfoReturnable<Double> cir) {
         // Infinityカード装着時は距離を問わず256mに設定
         if (wap.getInternalInventory().getStackInSlot(0).is(ModItems.INFINITY_CARD.get())) {
             cir.setReturnValue(256.0D);
+        }
+    }
+
+    @Inject(method = "getWapSqDistance", at = @At(value = "INVOKE", target = "appeng/blockentity/networking/WirelessAccessPointBlockEntity.getLocation()Lappeng/api/util/DimensionalBlockPos;"), cancellable = true)
+    private static void applied_ammo_box$mixinGetWapSqDistance$dimensionCardInject(WirelessAccessPointBlockEntity wap, BlockPos pos, Level level, CallbackInfoReturnable<Double> cir) {
+        // Dimensionカード装着時はディメンションを問わず1024mに設定
+        if (wap.getInternalInventory().getStackInSlot(0).is(ModItems.DIMENSION_CARD.get())) {
+            cir.setReturnValue(1024.0);
         }
     }
 }
